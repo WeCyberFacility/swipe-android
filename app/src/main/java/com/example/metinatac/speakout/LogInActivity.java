09913812ddc9999fa6b21856e1c8afde870f8225
Metin Atac <metin_a74@hotmail.de>
2018-11-03 21:29:21 +0100

    Metin All 03.11.18 21:17

--- app/src/main/java/com/example/metinatac/speakout/LogInActivity.java
+++ app/src/main/java/com/example/metinatac/speakout/LogInActivity.java
@@ -153,6 +153,7 @@ public class LogInActivity extends AppCompatActivity {
 
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
         // updateUI(currentUser);
         //  mGoogleApiClient.connect(); // <- Verursacht FEHLER (Crash)
         if (currentUser != null) {

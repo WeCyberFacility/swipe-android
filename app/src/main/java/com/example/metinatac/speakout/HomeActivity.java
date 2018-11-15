579882a77c0cdee235a409d21d9bf45d415669ab
Metin Atac <metin_a74@hotmail.de>
2018-11-15 19:43:52 +0100

    metin- drwaer dynamic

--- app/src/main/java/com/example/metinatac/speakout/HomeActivity.java
+++ app/src/main/java/com/example/metinatac/speakout/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.metinatac.speakout;
 
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,22 +15,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
+
+import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private FirebaseAuth mAuth;
+    ImageView  drawerPb;
+TextView userName;
+    private static final String TAG = "SignInACTIVITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
 
+
+        View headerView = navigationView.getHeaderView(0);
+
+
+userName = headerView.findViewById(R.id.nametxt);
+
+userName.setText(currentUser.getDisplayName());
+
+        drawerPb = headerView.findViewById(R.id.drawerpb);
         getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,
                 new HomeFragment()).commit();
 
-        int a = 0;
+
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -36,8 +67,16 @@ public class HomeActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
+
+       Uri profilePicture = currentUser.getPhotoUrl();
+
+        Picasso.get().load(profilePicture).transform(new CropCircleTransformation()).into(drawerPb);
+
+
+        Log.d(TAG,profilePicture.toString());
+
+
     }
 
     @Override

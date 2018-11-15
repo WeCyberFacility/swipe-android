cb5f15130f23490cdfb7a11747a16d0846fc99c6
Alpay <Alpaykucuk@outlook.de>
2018-11-15 18:23:34 +0100

    Alpay

--- app/src/main/java/com/example/metinatac/speakout/GoogleNutzer.java
+++ app/src/main/java/com/example/metinatac/speakout/GoogleNutzer.java
@@ -4,7 +4,6 @@ public class GoogleNutzer {
 
 
 
-
         private String id;
         private String name;
         private String nachname;
--- app/src/main/java/com/example/metinatac/speakout/HomeActivity.java
+++ app/src/main/java/com/example/metinatac/speakout/HomeActivity.java
@@ -23,14 +23,10 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,
+                new HomeFragment()).commit();
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,27 +48,7 @@ public class HomeActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -80,17 +56,15 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.profil) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.suchen) {
 
-        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.einstellungen) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.logout) {
 
         }
 

6c766b6ab7be628708f3c522b391203743251676
Metin Atac <metin_a74@hotmail.de>
2018-11-21 01:07:50 +0100

    metin- googleUserREg

---
+++ app/src/main/java/com/example/metinatac/speakout/GoogleUserRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.metinatac.speakout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GoogleUserRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_user_register);
+    }
+}
